56f906e26ee143968dd8e5a26b1d82a514551525
wkdtpgns5016 <wkdtpgns5016@naver.com>
2021-05-13 22:52:12 +0900

    Docs: 서점 시스템 프로젝트 생성
    스프링부트 프로젝트 생성

---
+++ src/main/java/com/example/BookstoreSystem/BookstoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookstoreSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSystemApplication.class, args);
+	}
+
+}
