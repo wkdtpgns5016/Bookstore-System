711fd06d95be5d5e5af8b0e4f2a87891da621050
wkdtpgns5016 <74850752+wkdtpgns5016@users.noreply.github.com>
2021-05-14 14:56:49 +0900

    Feat: MariaDB 연동 (#2)
    1. mariaDB 연동
    2. User 컨트롤러, 서비스, dao, dto 생성
    3. User data ReadAll 구현

---
+++ src/main/java/com/example/BookstoreSystem/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.BookstoreSystem.controller;
+
+import com.example.BookstoreSystem.model.UserDto;
+import com.example.BookstoreSystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
