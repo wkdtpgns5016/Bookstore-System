d391119f055560e8c06c54f312264ab2ab16d8ce
wkdtpgns5016 <74850752+wkdtpgns5016@users.noreply.github.com>
2021-06-21 18:31:46 +0800

    Feat: 도서 삽입 기능 추가 (#16)
    도서 삽입 기능 추가
    
    resolve #11

--- src/main/java/com/example/BookstoreSystem/service/BookService.java
+++ src/main/java/com/example/BookstoreSystem/service/BookService.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface BookService {
     public List<BookDto> selectBookList();
     public BookDto selectBookInfo(String bookId);
+    public int insertBookInfo(BookDto book);
 }
